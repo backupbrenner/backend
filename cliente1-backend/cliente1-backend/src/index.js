9e8ac20378b0f9d010dc28db8db21a9b8bc78a75
backupbrenner <38513523+backupbrenner@users.noreply.github.com>
2026-04-15 11:03:38 -0300

    Update index.js

--- cliente1-backend/cliente1-backend/src/index.js
+++ cliente1-backend/cliente1-backend/src/index.js
@@ -19,6 +19,6 @@ app.use("/api/menu", menuRoutes);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Backend rodando na porta", PORT);
 });
